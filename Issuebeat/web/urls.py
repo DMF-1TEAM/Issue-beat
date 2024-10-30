057ab342f90859c703c9b8e2808710d0565d51c4
EHYONGI <irene4618@naver.com>
2024-10-30 16:13:54 +0900

    result 링크 수정 홈 화면 연결

--- Issuebeat/web/urls.py
+++ Issuebeat/web/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import home, result, resultsam, news, news_chart
+from .views import home, resultsam, news, news_chart
 
 urlpatterns = [
     path('', home, name='home'),
-    path('<str:query>/result/', result, name='result'),
     path('result/', resultsam, name='resultsam'),
     path('news/', news, name='news'),
     path('chart/', news_chart, name='chart'),
