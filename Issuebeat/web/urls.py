5b3e68052a4f302c61951365907660233864d99f
EHYONGI <irene4618@naver.com>
2024-10-31 12:09:36 +0900

    병합된 파일 삭제

--- Issuebeat/web/urls.py
+++ Issuebeat/web/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import home, resultsam, news, news_chart
+from .views import home, resultsam
 
 urlpatterns = [
     path('', home, name='home'),
     path('result/', resultsam, name='resultsam'),
-    path('news/', news, name='news'),
-    path('chart/', news_chart, name='chart'),
 ]
