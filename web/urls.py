fe5e5348b2fc73b05de7bf7d7765d5ed7c463271
minwater96 <rlaalstn2568@naver.com>
2024-11-05 16:35:13 +0900

    mouseover 기능 업데이트

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'web'
 
@@ -12,4 +14,5 @@ urlpatterns = [
     path('api/trending/', views.get_trending_keywords_api, name='trending_keywords'),
     path('api/stats/daily/', views.get_daily_stats, name='daily_stats'),
     path('api/suggestions/', views.get_search_suggestions_api, name='suggestions'),
-]
+    path('api/news/hover-summary/<str:date>/', views.get_hover_summary, name='hover_summary'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
